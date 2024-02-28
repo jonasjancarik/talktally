d1addc61dd9bddfe0db6215bc60889863682704a
Jonáš Jančařík <jonas.jancarik@gmail.com>
2024-02-28 12:03:41 +0100

    Update basePath in next.config.mjs for development

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/talktally',
+    basePath: process.env.NODE_ENV === 'development' ? '' : '/talktally',
 };
 
 export default nextConfig;
