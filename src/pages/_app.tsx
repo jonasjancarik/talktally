692694956b85090c76b4687201a2b163f9ce0f60
Jonáš Jančařík <jonas.jancarik@gmail.com>
2024-02-28 16:40:11 +0100

    Add title and header to MyApp component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import '../styles/globals.css'; // Import other global styles
+import Head from 'next/head';
 
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>TalkTally</title>
+            </Head>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'></div>
+                    <h1 className="mt-3">TalkTally</h1>
+                </div>
+            </div>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
